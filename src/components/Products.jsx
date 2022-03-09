9c527817f36c27f510923e1147894ad785b09240
IG <iikim2511@gmail.com>
2022-03-09 18:19:37 -0500

    loading

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -8,32 +8,63 @@ const Products = () => {
 
   const [filter, setFilter] = useState(data);
 
+// 🍀js0309-0610. loading
+/* 🍄
+    fetch data 완료전에는 loading
+    fetch data 완료 후에는 ShowProducts
+*/
+
   const [loading, setLoading] = useState(false);
 
 
     useEffect(() => {
+
+        // 🧨js0309-0610. 
+        setLoading(true);
+
         fetch('https://fakestoreapi.com/products')
         .then(res=>res.json())
-        .then(json=>console.log(json))
+        .then(json=>console.log(json))    
 
-    return () => {
+        // 🧨js0309-0610. 
+        setLoading(false);
         
-    }
     }, [])
+
+    // 🧨js0309-0610. 
+    const Loading = () => {
+        return(
+            <div>
+                Loading....
+                
+            </div>
+        )
+    }
+
+    // 🧨js0309-0610. 
+    const ShowProducts = () => {
+        return (
+          <div>
+            <div className="button-container">
+              <button className="myBtn">All</button>
+              <button className="myBtn">Men</button>
+              <button className="myBtn">Women</button>
+              <button className="myBtn">Jewelery</button>
+              <button className="myBtn">Electronic</button>
+            </div>
+          </div>
+        );
+    }
 
 
     return (
         <div className="products">
 
-            <div className="button-container">
-                <button className="myBtn">All</button>
-                <button className="myBtn">Men</button>
-                <button className="myBtn">Women</button>
-                <button className="myBtn">Jewelery</button>
-                <button className="myBtn">Electronic</button>
-            
-            </div>
-            
+        
+        {/*🧨js0309-0610.  */}
+            {
+                loading ? <Loading/> : <ShowProducts/>
+            }
      
         
         </div>
