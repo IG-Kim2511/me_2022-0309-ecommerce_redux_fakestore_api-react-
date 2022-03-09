d66a72a12060eedbc545fa1a7bc690980d60d1ca
IG <iikim2511@gmail.com>
2022-03-09 15:08:12 -0500

    index.css

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <div className="home">home</div>
+        <div className="products">products</div>
+        <div className="cart">Cart</div>
+    
+    </div>
+  )
+}
+
+export default Nav
