a4cee4ed9cfa3fef630d4fe058e8bc272c143be7
IG <iikim2511@gmail.com>
2022-03-09 21:53:59 -0500

    products-flex

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,79 +1,76 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-import "./Products.css"
+import "./Products.css";
 
 const Products = () => {
+  // 🧨js0309-0630
   const [data, setData] = useState([]);
-
   const [filter, setFilter] = useState(data);
 
-// 🍀js0309-0610. loading
-/* 🍄
+  // 🍀js0309-0610. loading
+  /* 🍄
     fetch data 완료전에는 loading
     fetch data 완료 후에는 ShowProducts
 */
 
   const [loading, setLoading] = useState(false);
 
+  // 🍀js0309-0630. fetch data
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios("https://fakestoreapi.com/products");
 
-    useEffect(() => {
+      console.log(result);
+      console.log(result.data);
 
-        // 🧨js0309-0610. 
-        setLoading(true);
+      setData(result.data);
+      
+      setLoading(false);
+    };    
+    fetchItems();
+  }, []);
 
-        fetch('https://fakestoreapi.com/products')
-        .then(res=>res.json())
-        .then(json=>console.log(json))    
+  // 🧨js0309-0610.
+  const Loading = () => {
+    return <div>Loading....</div>;
+  };
 
-        // 🧨js0309-0610. 
-        setLoading(false);
-        
-    }, [])
-
-    // 🧨js0309-0610. 
-    const Loading = () => {
-        return(
-            <div>
-                Loading....
-                
-            </div>
-        )
-    }
-
-    // 🧨js0309-0610. 
-    const ShowProducts = () => {
-        return (
-          <div>
-            <div className="button-container">
-              <button className="myBtn">All</button>
-              <button className="myBtn">Men</button>
-              <button className="myBtn">Women</button>
-              <button className="myBtn">Jewelery</button>
-              <button className="myBtn">Electronic</button>
-            </div>
-          </div>
-        );
-    }
-
-
+  const ShowProducts = () => {
     return (
-        <div className="products">
-
-        
-        {/*🧨js0309-0610.  */}
-            {
-                loading ? <Loading/> : <ShowProducts/>
-            }
-     
-        
+      <div>
+        {/* 🧨js0309-0610.  */}
+        <div className="button-container">
+          <button className="myBtn">All</button>
+          <button className="myBtn">Men</button>
+          <button className="myBtn">Women</button>
+          <button className="myBtn">Jewelery</button>
+          <button className="myBtn">Electronic</button>
         </div>
-      )
+
+        <div className="container">
+            {/* 🧨js0309-0630.  */}
+            {data.map((data_item) => (
+            <div className="items-container" key={data_item.id}>
+                <div className="items">
+                <div className="img-parent">
+                    <img src={data_item.image} />
+                </div>
+                <div className="items">{data_item.title}</div>
+                </div>
+            </div>
+            ))}
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="products">
+      {/*🧨js0309-0610.  */}
+      {loading ? <Loading /> : <ShowProducts />}
+    </div>
+  );
 };
-
-
-
-
-
 
 export default Products;
