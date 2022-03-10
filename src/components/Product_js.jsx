199015c191f46fed094ad41abb10d0652abd71e7
IG <iikim2511@gmail.com>
2022-03-10 16:13:30 -0500

    product_js

--- src/components/Product_js.jsx
+++ src/components/Product_js.jsx
@@ -1,71 +1,76 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
+import "./Product_js.css";
 
 const Product_js = () => {
+  // 🍀0310-0240 useParams,
+  const { id } = useParams();
 
-    // 🍀0310-0240 useParams, 
-    const {id} =useParams()
+  // 🥒0310-0230, fetch api
+  const [data_product, setData_product] = useState([]);
 
-    // 🥒0310-0230, fetch api
-    const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const [loading, setLoading] = useState(false)
-
-    // 🍀0310-0230, fetch api, 
-    /* 🍄
+  // 🍀0310-0230, fetch api,
+  /* 🍄
       10. product.jsx의 fetch code 복붙
       15. const {id} =useParams()
       20. api address `` 으로 바꿈, 
       30. api address: ~~~~/${id} 추가    🦄
     */
-    useEffect(() => {
-        const fetchItems = async () => {
-    
-          setLoading(true);
-    
-        // 🥒0310-0240
-          const result = await axios(`https://fakestoreapi.com/products/${id}`);
-    
-          console.log(result.data);
-    
-          setProduct(result.data);         
-    
-          setLoading(false);
-        };
-        fetchItems();
-      }, []);
-    
-    
-      const Loading = () => {
-        return(
-          <div>
-            <h1>Loading...</h1>
-          </div>
-          )
-      }
+  useEffect(() => {
+    const fetchItems = async () => {
+      setLoading(true);
 
-       // 🍀0310-0255, each product rendering 👉Products.jsx
-       /* 🦄
+      // 🥒0310-0240
+      const result = await axios(`https://fakestoreapi.com/products/${id}`);
+
+      console.log(result.data);
+
+      setData_product(result.data);
+
+      setLoading(false);
+    };
+    fetchItems();
+  }, []);
+
+  const Loading = () => {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  };
+
+  // 🍀0310-0255, each product rendering 👉Products.jsx
+  /* 🦄
         return (~~) 빼먹지말고 꼭 넣을것
        */
-      const ShowProduct = () => {
-        return(
-          <div>
-            <div>            
-              <img src={product.image} alt="" />
-            </div>          
+  const ShowProduct = () => {
+    return (
+      <div>
+        <div className="items" key={data_product.id}>
+          <div className="item">
+            <div className="img-parent">
+              <img src={data_product.image} alt={data_product.title} />
+            </div>
           </div>
-          )
-      }
+          <div className="item">
+            <div className="title">Name: {data_product.title}</div>
+            <div className="price">$ {data_product.price}</div>
 
-      return (
-        <div className="product_js">        
-        
-          {loading ? <Loading/> : <ShowProduct/>}
+            <div className="description">Desc: {data_product.description}</div>
+          </div>
         </div>
-      )
-}
+      </div>
+    );
+  };
 
-export default Product_js
+  return (
+    <div className="product_js">{loading ? <Loading /> : <ShowProduct />}</div>
+  );
+};
+
+export default Product_js;
